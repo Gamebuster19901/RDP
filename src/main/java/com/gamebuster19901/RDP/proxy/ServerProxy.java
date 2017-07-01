fdfe6af233c3f7aee6a0777e871aa9e82c3bc88a
Choonster TheMage <Choonster.2010@gmail.com>
2017-07-01 21:50:07 +1000

    Remove unused imports

--- src/main/java/com/gamebuster19901/RDP/proxy/ServerProxy.java
+++ src/main/java/com/gamebuster19901/RDP/proxy/ServerProxy.java
@@ -1,8 +1,5 @@
 package com.gamebuster19901.RDP.proxy;
 
-import net.minecraft.client.settings.KeyBinding;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
