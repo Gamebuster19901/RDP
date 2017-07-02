c5bcd705133d557e9b25207ec21ae0f37ac87a8e
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-07-02 16:20:06 -0400

    Changed to for loop to make adding new keybindings easier and for readability.

--- src/main/java/com/gamebuster19901/RDP/proxy/key/KeyEventHandler.java
+++ src/main/java/com/gamebuster19901/RDP/proxy/key/KeyEventHandler.java
@@ -1,98 +1,40 @@
 package com.gamebuster19901.RDP.proxy.key;
 
+import static com.gamebuster19901.RDP.proxy.key.ClientProxy.KEYBINDINGS;
+import static com.gamebuster19901.RDP.proxy.key.KeyEvent.EventType.HELD;
+import static com.gamebuster19901.RDP.proxy.key.KeyEvent.EventType.PUSHED;
+import static com.gamebuster19901.RDP.proxy.key.KeyEvent.EventType.RELEASED;
+
 import java.util.LinkedHashSet;
 
 import com.gamebuster19901.RDP.event.KeyListener;
 
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
-import static com.gamebuster19901.RDP.proxy.key.ClientProxy.KEYBINDINGS;
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.EventType.HELD;
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.EventType.PUSHED;
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.EventType.RELEASED;
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.KeyType.DOWN;
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.KeyType.LEFT;
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.KeyType.RIGHT;
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.KeyType.UP;
 
 public class KeyEventHandler {
 	public static final KeyEventHandler INSTANCE = new KeyEventHandler();
 	private static final LinkedHashSet<KeyListener> LISTENERS = new LinkedHashSet<KeyListener>();
 	
 	public final void onKeyEvent(){
-		if(ClientProxy.KEYBINDINGS[0].isPressed()){
-			if(KEYBINDINGS[0].wasPressed == false){
-				KEYBINDINGS[0].wasPressed = true;
-				new KeyEvent(PUSHED, UP);
-				KEYBINDINGS[0].cyclesHeld++;
+		for(AbsoluteKeyBinding kb : KEYBINDINGS){
+			if(kb.isPressed()){
+				if (kb.wasPressed == false){
+					kb.wasPressed = true;
+					new KeyEvent(PUSHED, KeyType.get(kb.getKeyCode()));
+					kb.cyclesHeld++;
+				}
+				else if (kb.wasPressed == true){
+					new KeyEvent(HELD, KeyType.get(kb.getKeyCode()));
+					kb.cyclesHeld++;
+				}
 			}
-			else if (KEYBINDINGS[0].wasPressed == true){
-				new KeyEvent(HELD, UP);
-				KEYBINDINGS[0].cyclesHeld++;
-			}
-		}
-		else if(!(ClientProxy.KEYBINDINGS[0].isPressed())){
-			if(KEYBINDINGS[0].wasPressed){
-				KEYBINDINGS[0].wasPressed = false;
-				new KeyEvent(RELEASED, UP);
-				KEYBINDINGS[0].cyclesHeld = 0;
-			}
-		}
-		
-		if(ClientProxy.KEYBINDINGS[1].isPressed()){
-			if(KEYBINDINGS[1].wasPressed == false){
-				KEYBINDINGS[1].wasPressed = true;
-				new KeyEvent(PUSHED, RIGHT);
-				KEYBINDINGS[1].cyclesHeld++;
-			}
-			else if (KEYBINDINGS[1].wasPressed == true){
-				new KeyEvent(HELD, RIGHT);
-				KEYBINDINGS[1].cyclesHeld++;
-			}
-		}
-		else if(!(ClientProxy.KEYBINDINGS[1].isPressed())){
-			if(KEYBINDINGS[1].wasPressed){
-				KEYBINDINGS[1].wasPressed = false;
-				new KeyEvent(RELEASED, RIGHT);
-				KEYBINDINGS[1].cyclesHeld = 0;
-			}
-		}
-		
-		if(ClientProxy.KEYBINDINGS[2].isPressed()){
-			if(KEYBINDINGS[2].wasPressed == false){
-				KEYBINDINGS[2].wasPressed = true;
-				new KeyEvent(PUSHED, DOWN);
-				KEYBINDINGS[2].cyclesHeld++;
-			}
-			else if (KEYBINDINGS[2].wasPressed == true){
-				new KeyEvent(HELD, DOWN);
-				KEYBINDINGS[2].cyclesHeld++;
-			}
-		}
-		else if(!(ClientProxy.KEYBINDINGS[2].isPressed())){
-			if(KEYBINDINGS[2].wasPressed){
-				KEYBINDINGS[2].wasPressed = false;
-				new KeyEvent(RELEASED, DOWN);
-				KEYBINDINGS[2].cyclesHeld = 0;
-			}
-		}
-		
-		if(ClientProxy.KEYBINDINGS[3].isPressed()){
-			if(KEYBINDINGS[3].wasPressed == false){
-				KEYBINDINGS[3].wasPressed = true;
-				new KeyEvent(PUSHED, LEFT);
-				KEYBINDINGS[3].cyclesHeld++;
-			}
-			else if (KEYBINDINGS[3].wasPressed == true){
-				new KeyEvent(HELD, DOWN);
-				KEYBINDINGS[3].cyclesHeld++;
-			}
-		}
-		else if(!(ClientProxy.KEYBINDINGS[3].isPressed())){
-			if(KEYBINDINGS[3].wasPressed){
-				KEYBINDINGS[3].wasPressed = false;
-				new KeyEvent(RELEASED, UP);
-				KEYBINDINGS[3].cyclesHeld = 0;
+			else{
+				if(kb.wasPressed){
+					kb.wasPressed = false;
+					new KeyEvent(RELEASED, KeyType.get(kb.getKeyCode()));
+					kb.cyclesHeld = 0;
+				}
 			}
 		}
 	}
