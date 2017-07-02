601e8ed1ee1e50ac2b13e205074711a05324a208
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-07-02 16:24:23 -0400

    uncommented code, readded import

--- src/main/java/com/gamebuster19901/RDP/event/KeyListener.java
+++ src/main/java/com/gamebuster19901/RDP/event/KeyListener.java
@@ -1,11 +1,12 @@
 package com.gamebuster19901.RDP.event;
 
 import com.gamebuster19901.RDP.proxy.key.KeyEvent;
+import com.gamebuster19901.RDP.proxy.key.KeyEventHandler;
 
 public interface KeyListener {
 	public void onKeyEvent(KeyEvent e);
 	
 	default void register(){
-		//KeyEventHandler.INSTANCE.register(this);
+		KeyEventHandler.INSTANCE.register(this);
 	}
 }
