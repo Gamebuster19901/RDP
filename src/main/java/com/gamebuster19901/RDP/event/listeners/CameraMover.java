071306868003f401331f5296696cf34ba2ab2153
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-07-02 16:17:39 -0400

    Remove unused imports, added TODO

--- src/main/java/com/gamebuster19901/RDP/event/listeners/CameraMover.java
+++ src/main/java/com/gamebuster19901/RDP/event/listeners/CameraMover.java
@@ -1,17 +1,7 @@
 package com.gamebuster19901.RDP.event.listeners;
 
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.EventType.HELD;
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.EventType.PUSHED;
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.EventType.RELEASED;
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.KeyType.DOWN;
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.KeyType.LEFT;
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.KeyType.RIGHT;
-import static com.gamebuster19901.RDP.proxy.key.KeyEvent.KeyType.UP;
-
 import com.gamebuster19901.RDP.event.KeyListener;
 import com.gamebuster19901.RDP.proxy.key.KeyEvent;
-
-import net.minecraft.client.Minecraft;
 
 public class CameraMover implements KeyListener{
 	
@@ -21,11 +11,7 @@ public class CameraMover implements KeyListener{
 	
 	@Override
 	public void onKeyEvent(KeyEvent e) {
-		if(e.getKeyType().equals(UP)){
-			if(e.getEventType().equals(PUSHED) || e.getEventType().equals(HELD)){
-				Minecraft.getMinecraft().player.cameraYaw++;
-			}
-		}
+		//TODO for loop
 		
 	}
 	
