bb61e170d7e9f7821d00fa678317dbe3cbe8d1cc
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2018-01-09 01:52:40 -0500

    Remove unused exception.

--- src/main/java/com/gamebuster19901/RDP/exception/SingletonException.java
+++
@@ -1,7 +0,0 @@
-package com.gamebuster19901.RDP.exception;
-
-public class SingletonException extends RuntimeException {
-	public SingletonException(Class c){
-		super("Cannot instantiate more than one instance of " + c.getSimpleName());
-	}
-}
